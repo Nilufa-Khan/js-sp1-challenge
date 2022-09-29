899dfbdee844a3142dcfdf3bf33cca96fbba4b65
Nilufa Khan <khannilufa5@gmail.com>
2022-09-29 20:39:21 +0530

    initial

--- c2-lucky-number/c2-submission.js
+++ c2-lucky-number/c2-submission.js
@@ -1,4 +1,22 @@
 let n = 31052022;
 // Write solution code here to find the lucky number
 
+let sum = 0;
+let sum2 = 0;
+while(n!= 0){
+    
+    sum=sum + (n % 10);
+   
+        sum2 = sum2 + (sum % 10);
+        sum = sum / 10;
+    
+    n = n /10;
+  
+    
+}
+
+
+
 console.log(`Your Lucky Number is ${sum}`);
+console.log(`Your Lucky Number is ${sum2}`);
+    
